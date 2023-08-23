61c63eb05527a2c924c9dcef0683b48f423b40c0
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-08-23 14:07:36 +0000

    done

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,7 +1,7 @@
-import ClassComponent from "./components/ClassComponent";
-import FunctionalComponent from "./components/FunctionalComponent";
+import ClassComponent from "./pages/ClassComponent";
+import FunctionalComponent from "./pages/FunctionalComponent";
 import { useState } from "react";
-
+import {Routes, Route} from "react-router-dom";
 
 
 function App() {
@@ -12,14 +12,18 @@ function App() {
   const [nameConfig, setNameConfig] = useState("defalutValue")
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Functional Component</h1>
-        <ClassComponent />
-        <FunctionalComponent name={name} age={10} company={company} setName={setName}/>
-        <FunctionalComponent nameConfig={nameConfig} age={10} company={company} setNameConfig={setNameConfig}/>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<ClassComponent />}/>
+      <Route path="/functional-component" element={<FunctionalComponent />} />
+    </Routes>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>Functional Component</h1>
+    //     <ClassComponent />
+    //     <FunctionalComponent name={name} age={10} company={company} setName={setName}/>
+    //     <FunctionalComponent nameConfig={nameConfig} age={10} company={company} setNameConfig={setNameConfig}/>
+    //   </header>
+    // </div>
   );
 }
 
